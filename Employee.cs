59113a6530014d0a5b5d07f502b132cb8c5a46ff
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-03-12 16:22:24 -0600

    Employee Class

--- Employee.cs
+++ Employee.cs
@@ -14,6 +14,51 @@ namespace MidtermProject {
     //     Gets the employee's empType
     //     Sets the employee's empType (empType should be an enumerated type called EmployeeType where the fields are SalesPerson, Manager, Production)
     //     Gets the employee's id
+    class Employee {
+        private string firstName;
+        private string lastName;
+        private string id;
+        private EmployeeType empType;
 
+        public Employee(string firstName, string lastName, string id, EmployeeType empType) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+            this.empType = empType;
+        }
 
+        public void setFirstName(string input) {
+            this.firstName = input;
+        }
+
+        public string getFirstName() {
+            return this.firstName;
+        }
+
+        public void setLastName(string input) {
+            this.lastName = input;
+        }
+
+        public string getLastName() {
+            return this.lastName;
+        }
+
+        public string getID() {
+            return this.id;
+        }
+
+        public void setEmpType(EmployeeType input) {
+            this.empType = input;
+        }
+
+        public EmployeeType GetEmployeeType() {
+            return this.empType;
+        }
+
+        public void getEmployeeInfo() {
+            System.Console.WriteLine($"Name: { this.firstName } { this.lastName }");
+            System.Console.WriteLine($"ID: { this.id }");
+            System.Console.WriteLine($"Employee Type: { this.empType }");
+        }
+    }
 }

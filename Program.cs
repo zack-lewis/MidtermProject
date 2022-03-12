e8ede1fd22d4d7dd442299685bd4e84db88c1f83
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-03-12 16:01:29 -0600

    Project initial info from challenge

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace MidtermProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create instances of Employee, SalesPerson, and Manager
+            Employee employee1 = new Employee("Truman", "Tiger", "12345", EmployeeType.Sales);
+            SalesPerson salesPerson1 = new SalesPerson("Mickey", "Mouse", "23456", "Sporting Goods", 7500);
+            Manager manager1 = new Manager("Elmer", "Fudd", "56789", "Electronics", "Midwest");
+
+            Console.WriteLine("\n-------Employee 1-------------");
+            employee1.getEmployeeInfo();
+
+            Console.WriteLine("\n-------Sales Person 1-------------");
+            salesPerson1.getEmployeeInfo();
+            Console.WriteLine($"Sales Level: {salesPerson1.GetSalesLevel()} | Sales: ${salesPerson1.getSales()}");
+            salesPerson1.updateSales(5250.70f);
+            Console.WriteLine($"Updated Sales Level: {salesPerson1.GetSalesLevel()} | Updated Sales: ${salesPerson1.getSales()}");
+
+            Console.WriteLine("\n-------Manager 1-------------");
+            manager1.getEmployeeInfo();
+            Console.WriteLine($"Dept: {manager1.getDepartment()} | Region: {manager1.getRegion()}");
+            manager1.setFirstName("Wiley");
+            manager1.setLastName("Coyote");
+            manager1.setRegion("Southeast");
+            manager1.setDepartment("Automotive");
+            Console.WriteLine($"\nNew Name: {manager1.getFirstName()} {manager1.getlastName()}");
+            Console.WriteLine($"New Dept: {manager1.getDepartment()} | New Region: {manager1.getRegion()}");
         }
     }
 }
